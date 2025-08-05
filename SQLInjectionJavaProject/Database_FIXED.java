5ccbc42eb1816d21645bc2b0127227a8d0ff92ee
gr8goat <90510480+toutbien@users.noreply.github.com>
2025-08-05 15:29:56 +0000

    adding the data needed in Database_FIXED

--- SQLInjectionJavaProject/Database_FIXED.java
+++ SQLInjectionJavaProject/Database_FIXED.java
@@ -1,1 +1,22 @@
 
+
+package db;
+
+import java.sql.*;
+
+public class Database {
+    public boolean loginUser(String username, String password) {
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:users.db")) {
+            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next(); // returns true if user found
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
