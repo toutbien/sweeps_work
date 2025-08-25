1811c2f0751dd9713ca5860fa17fe13eec65be0a
gr8goat <90510480+toutbien@users.noreply.github.com>
2025-08-25 13:56:05 -0400

    Add files via upload

---
+++ Module9/ch12-animal-sim/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
